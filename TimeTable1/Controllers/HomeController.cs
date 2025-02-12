02c961747e255a74e7300014962a1d99defd5587
archana800 <152801657+archana800@users.noreply.github.com>
2025-02-12 12:00:19 +0530

    Add project files.

---
+++ TimeTable1/Controllers/HomeController.cs
@@ -0,0 +1,51 @@
+﻿using System.Web.Mvc;
+using TimeTable1.Models;
+
+namespace TimeTable1.Controllers
+{
+    public class HomeController : Controller
+    {
+        #region Methods
+
+        public ActionResult Index(StudentSub inputFields)
+        {
+
+            if (inputFields.Total_hours_for_week != null && inputFields.Total_hours_for_week > 0)
+            {
+                if (inputFields.Total_Subjects == 1)
+                {
+                    inputFields.Sub_Working_Hours = inputFields.Total_hours_for_week;
+                }
+                else if (inputFields.Total_Subjects == 2)
+                {
+                    int total = inputFields.Total_hours_for_week / inputFields.Total_Subjects;
+                    inputFields.Sub_Working_Hours = total;
+
+                }
+                else if (inputFields.Total_Subjects == 3)
+                {
+
+                    int total = inputFields.Total_hours_for_week / inputFields.Total_Subjects;
+                    inputFields.Sub_Working_Hours = total;
+                }
+                else
+                {
+
+                    int total = inputFields.Total_hours_for_week / inputFields.Total_Subjects;
+                    inputFields.Sub_Working_Hours = total;
+                }
+                return RedirectToAction("GeneratSubject", inputFields);
+            }
+
+            return View();
+        }
+
+        public ActionResult GeneratSubject(StudentSub inputFields)
+        {
+            return View(inputFields);
+        }
+
+    }
+
+    #endregion
+}
---
+++ TimeTable1/Models/StudentSub.cs
@@ -0,0 +1,26 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace TimeTable1.Models
+{
+    public class StudentSub
+    {
+        #region properties
+
+        public int Sub_Working_Hours { get; set; }
+
+        public int Id { get; set; }
+
+        [Required(ErrorMessage = "No of Working days is required")]
+        public int No_of_Working_days { get; set; }
+
+        [Required(ErrorMessage = "No of Subjects per day is required")]
+        public int No_of_Subjects_per_day { get; set; }
+
+        [Required(ErrorMessage = "Total Subjects is required")]
+        public int Total_Subjects { get; set; }
+
+        public int Total_hours_for_week { get; set; }
+
+        #endregion
+    }
+}
